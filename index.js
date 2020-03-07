69a8f4c45068e2bf58d149eef95a0a66f78e00c0
mstokeley <matthewjaestokeley@gmail.com>
2020-03-07 10:37:08 -0500

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+export var _cache = function( key ) {
+
+	let map = new WeakMap()
+
+	return map.contains( key )
+		? map.get( key )
+		: map.add( key )
+
+}
+
+export var resolverKeyStrategy = function( _cache, fn ) {
+
+	return function( str ) {
+		return _cache ( fn.call ( this, str ) ) 
+	}
+
+}
---
+++ tests/index.spec.js
@@ -0,0 +1,25 @@
+import { resolverKeyStrategy, _cache } from '../index'
+
+describe ( 'resolverKeyStrategy', () => {
+
+	it( 'should return a resolved key ', () => {
+
+		const fn = ( str ) => {
+
+			return ( key ) => {
+				return str + key
+			}
+
+		}
+
+		let _prepareKey = fn( 'prefix' )
+
+		let key = _prepareKey( 'key' )
+
+		let _keystore = _symbolKeyStrategy( _cache )
+
+		expect( _keystore( key ) ).toEqual( key )
+
+	} )
+
+} )
